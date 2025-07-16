0de792a76c8a68da9e62c6d0fb91224d2a59f478
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-07-16 16:45:52 +0800

    commit

--- transcriber/src/listener.py
+++ transcriber/src/listener.py
@@ -2,7 +2,7 @@ import os
 
 from public_configs.project_config import get_project_root
 from public_utils.terminal_logger.logger import info
-from transcriber.src.utils.command_generator import convert_events_to_drawing_commands, print_command
+# from transcriber.src.utils.command_generator import convert_events_to_drawing_commands, print_command
 from transcriber.src.utils.mouse_recorder import record_mouse, print_record, export_to_file
 
 file_path = os.path.join(get_project_root(), "transcriber", "test", "resources", "json", "mouse_event.json")
@@ -16,6 +16,6 @@ if __name__ == "__main__":
 
     # export_to_file(mouse_records, file_path)
 
-    commands = convert_events_to_drawing_commands(mouse_records)
-
-    print_command(commands)
+    # commands = convert_events_to_drawing_commands(mouse_records)
+    #
+    # print_command(commands)
